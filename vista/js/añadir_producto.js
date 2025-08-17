8840e19a29d2c6762998a50022d53424b0b3cde6
Danielj2005 <dbarrueta42@gmail.com>
2025-08-17 12:24:30 -0400

    restauracion de añadir producto js

---
+++ vista/js/añadir_producto.js
@@ -0,0 +1,20 @@
+/* 
+@{btn_add_card_product}
+- obtiene y almacena el id del boton.
+- al darle click agregará una card para registrar otro producto
+*/
+const btn_add_card_product = document.getElementById('btn_add_card_product');
+btn_add_card_product.addEventListener('click', (e) => {
+    e.preventDefault();
+    $.ajax({
+        data: '',
+        url:  "../include/añadir_producto.php",
+        type:  'post',
+        success:function(valores){
+            $(`#tableProduct`).append(valores);
+        },
+        error: function(){
+            Swal.fire("ocurrio un error!","la solicitud no pudo ser procesada","error");
+        }
+    });
+});
